b1cfd347b65b7c88a2c8d6e4c9b0ac19148bc6bc
James McCarty <jameswmccarty@gmail.com>
2019-12-06 19:20:21 -0500

    Update Day 06 file (Part 2)

--- day06.py
+++ day06.py
@@ -113,6 +113,7 @@ Although it hasn't changed, you can still get your puzzle input.
 """
 
 orbits = dict()
+seen = set()
 
 def body_total(b):
 	count = 0
@@ -123,6 +124,28 @@ def body_total(b):
 		else:
 			break
 	return count
+
+def search(source, dest, step):
+	if source == '' or orbits[source] == []:
+		return float('inf')
+	if source == dest:
+		return step
+	l = orbits[source][0]
+	r = []
+	for key, value in orbits.items():
+		if source in value:
+			r.append(key)
+	seen.add(source)
+	l_search = float('inf')
+	r_search = [float('inf')]
+	if r != []:
+		for rr in r:
+			if rr not in seen:
+				r_search.append(search(rr, dest, step + 1))
+	r_search = min(r_search)
+	if l != '' and l not in seen:
+		l_search = search(l, dest, step + 1)
+	return min(l_search, r_search)
 	
 
 if __name__ == "__main__":
@@ -139,3 +162,6 @@ if __name__ == "__main__":
 			orbits[b].append(a)
 		
 	print(sum((body_total(x) for x in orbits.keys())))
+
+	# Part 2 Solution
+	print(search('YOU', 'SAN', 0)-2)
