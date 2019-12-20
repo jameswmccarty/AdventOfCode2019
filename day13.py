3092593d8111d833c8d8ca82f324c030357542a6
James McCarty <jameswmccarty@gmail.com>
2019-12-20 17:57:50 -0500

    Add Day 13 files (Part 1)

---
+++ day13.py
@@ -0,0 +1,295 @@
+#!/usr/bin/python
+
+"""
+--- Day 13: Care Package ---
+
+As you ponder the solitude of space and the ever-increasing three-hour roundtrip for messages between you and Earth, you notice that the Space Mail Indicator Light is blinking. To help keep you sane, the Elves have sent you a care package.
+
+It's a new game for the ship's arcade cabinet! Unfortunately, the arcade is all the way on the other end of the ship. Surely, it won't be hard to build your own - the care package even comes with schematics.
+
+The arcade cabinet runs Intcode software like the game the Elves sent (your puzzle input). It has a primitive screen capable of drawing square tiles on a grid. The software draws tiles to the screen with output instructions: every three output instructions specify the x position (distance from the left), y position (distance from the top), and tile id. The tile id is interpreted as follows:
+
+    0 is an empty tile. No game object appears in this tile.
+    1 is a wall tile. Walls are indestructible barriers.
+    2 is a block tile. Blocks can be broken by the ball.
+    3 is a horizontal paddle tile. The paddle is indestructible.
+    4 is a ball tile. The ball moves diagonally and bounces off objects.
+
+For example, a sequence of output values like 1,2,3,6,5,4 would draw a horizontal paddle tile (1 tile from the left and 2 tiles from the top) and a ball tile (6 tiles from the left and 5 tiles from the top).
+
+Start the game. How many block tiles are on the screen when the game exits?
+
+--- Part Two ---
+
+The game didn't run because you didn't put in any quarters. Unfortunately, you did not bring any quarters. Memory address 0 represents the number of quarters that have been inserted; set it to 2 to play for free.
+
+The arcade cabinet has a joystick that can move left and right. The software reads the position of the joystick with input instructions:
+
+    If the joystick is in the neutral position, provide 0.
+    If the joystick is tilted to the left, provide -1.
+    If the joystick is tilted to the right, provide 1.
+
+The arcade cabinet also has a segment display capable of showing a single number that represents the player's current score. When three output instructions specify X=-1, Y=0, the third output instruction is not a tile; the value instead specifies the new score to show in the segment display. For example, a sequence of output values like -1,0,12345 would show 12345 as the player's current score.
+
+Beat the game by breaking all the blocks. What is your score after the last block is broken?
+
+"""
+
+class IntPuterVM:
+	
+	def __init__(self, memory):
+		self.mem = memory
+		self.mem += [0] * 5000
+		self.last_out = None
+		self.prog_in = []
+		self.halted = False
+		self.blocked = True
+		self.ip_state = 0
+		self.base = 0
+
+	def buffer_read(self, i):
+		self.prog_in.append(i)
+		self.blocked = False
+
+	def run(self):
+		ip = self.ip_state
+		while True:
+			A = None
+			B = None
+			C = None
+			d_ip = None
+			ip_updated = False
+			op = self.mem[ip]
+			modes, opcode = str(op)[:-2], int(str(op)[-2:])
+			if opcode == 99: # Halt
+				self.halted = True
+				return
+			if opcode == 1: # res = C + B
+				d_ip = 4
+				if modes == '':
+					self.mem[self.mem[ip+3]] = self.mem[self.mem[ip+1]] + self.mem[self.mem[ip+2]]
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					modes = modes[:-1]
+					if modes == '' or modes[-1] == '0':
+						self.mem[self.mem[ip+3]] = C + B
+					elif modes == '2':
+						self.mem[self.base+self.mem[ip+3]] = C + B
+			elif opcode == 2: # res = C * B
+				d_ip = 4
+				if modes == '':
+					self.mem[self.mem[ip+3]] = self.mem[self.mem[ip+1]] * self.mem[self.mem[ip+2]]
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					modes = modes[:-1]
+					if modes == '' or modes[-1] == '0':
+						self.mem[self.mem[ip+3]] = C * B
+					elif modes[-1] == '2':
+						self.mem[self.base+self.mem[ip+3]] = C * B
+			elif opcode == 3: # Store input at param 1
+				d_ip = 2
+				if len(self.prog_in) == 0:
+					self.blocked = True
+					self.ip_state = ip
+					return
+				if modes == '' or modes == '0':
+					self.mem[self.mem[ip+1]] = self.prog_in.pop(0)
+				elif modes == '1':
+					self.mem[ip+1] = self.prog_in.pop(0)
+				elif modes == '2':
+					self.mem[self.base+self.mem[ip+1]] = self.prog_in.pop(0)
+			elif opcode == 4: # Output value at param 1
+				d_ip = 2
+				if modes == '' or modes == '0':
+					self.last_out = self.mem[self.mem[ip+1]]
+				elif modes == '1':
+					self.last_out = self.mem[ip+1]
+				elif modes == '2':
+					self.last_out = self.mem[self.base+self.mem[ip+1]]
+				yield self.last_out
+			elif opcode == 5: # Jump if True (non-zero)
+				d_ip = 3
+				if modes == '':
+					if self.mem[self.mem[ip+1]] != 0:
+						ip = self.mem[self.mem[ip+2]]
+						ip_updated = True
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					modes = modes[:-1]
+					if C != 0:
+						ip = B
+						ip_updated = True
+			elif opcode == 6: # Jump if False (zero)
+				d_ip = 3
+				if modes == '':
+					if self.mem[self.mem[ip+1]] == 0:
+						ip = self.mem[self.mem[ip+2]]
+						ip_updated = True
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					modes = modes[:-1]
+					if C == 0:
+						ip = B
+						ip_updated = True
+			elif opcode == 7: # res = C < B
+				d_ip = 4
+				if modes == '':
+					if self.mem[self.mem[ip+1]] < self.mem[self.mem[ip+2]]:
+						self.mem[self.mem[ip+3]] = 1
+					else:
+						self.mem[self.mem[ip+3]] = 0
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					modes = modes[:-1]
+					if C < B:
+						if modes == '' or modes[-1] == '0':
+							self.mem[self.mem[ip+3]] = 1
+						elif modes[-1] == '2':
+							self.mem[self.base+self.mem[ip+3]] = 1
+					else:
+						if modes == '' or modes[-1] == '0':
+							self.mem[self.mem[ip+3]] = 0
+						elif modes[-1] == '2':
+							self.mem[self.base+self.mem[ip+3]] = 0
+			elif opcode == 8: # res = C == B
+				d_ip = 4
+				if modes == '':
+					if self.mem[self.mem[ip+1]] == self.mem[self.mem[ip+2]]:
+						self.mem[self.mem[ip+3]] = 1
+					else:
+						self.mem[self.mem[ip+3]] = 0
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					modes = modes[:-1]
+					if C == B:
+						if modes == '' or modes[-1] == '0':
+							self.mem[self.mem[ip+3]] = 1
+						elif modes[-1] == '2':
+							self.mem[self.base+self.mem[ip+3]] = 1
+					else:
+						if modes == '' or modes[-1] == '0':
+							self.mem[self.mem[ip+3]] = 0
+						elif modes[-1] == '2':
+							self.mem[self.base+self.mem[ip+3]] = 0
+			elif opcode == 9: # adjust base pointer by param
+				d_ip = 2
+				if modes == '':
+					self.base += self.mem[self.mem[ip+1]]
+				else:
+					if modes[-1] == '1':
+						self.base += self.mem[ip+1]
+					elif modes[-1] == '0':
+						self.base += self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						self.base += self.mem[self.base+self.mem[ip+1]]
+			if not ip_updated:
+				ip += d_ip
+
+def run_arcade(vm):
+	outs = []
+	screen = dict()
+	while not vm.halted:
+		for o in vm.run():
+			outs.append(o)
+		while len(outs) >= 3:
+			pixel = outs[0:3]
+			outs = outs[3:]
+			x = pixel[0]
+			y = pixel[1]
+			p = pixel[2]
+			screen[(x,y)] = p
+	return screen
+
+if __name__ == "__main__":
+
+	# Part 1 Solution
+	with open("day13_input", 'r') as infile:
+		prog = [ int(x) for x in infile.readline().strip().split(',') ]
+		vm = IntPuterVM(prog[:])
+		screen = run_arcade(vm)
+		print(list(screen.values()).count(2))
+	
