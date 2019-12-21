1d7fca5f360225aed8c7e56cbbd2a8eea1bdd288
James McCarty <jameswmccarty@gmail.com>
2019-12-21 17:06:06 -0500

    Add Day 15 files (Part 1)

---
+++ day15.py
@@ -0,0 +1,423 @@
+#!/usr/bin/python
+
+from copy import deepcopy
+
+"""
+--- Day 15: Oxygen System ---
+
+Out here in deep space, many things can go wrong. Fortunately, many of those things have indicator lights. Unfortunately, one of those lights is lit: the oxygen system for part of the ship has failed!
+
+According to the readouts, the oxygen system must have failed days ago after a rupture in oxygen tank two; that section of the ship was automatically sealed once oxygen levels went dangerously low. A single remotely-operated repair droid is your only option for fixing the oxygen system.
+
+The Elves' care package included an Intcode program (your puzzle input) that you can use to remotely control the repair droid. By running that program, you can direct the repair droid to the oxygen system and fix the problem.
+
+The remote control program executes the following steps in a loop forever:
+
+    Accept a movement command via an input instruction.
+    Send the movement command to the repair droid.
+    Wait for the repair droid to finish the movement operation.
+    Report on the status of the repair droid via an output instruction.
+
+Only four movement commands are understood: north (1), south (2), west (3), and east (4). Any other command is invalid. The movements differ in direction, but not in distance: in a long enough east-west hallway, a series of commands like 4,4,4,4,3,3,3,3 would leave the repair droid back where it started.
+
+The repair droid can reply with any of the following status codes:
+
+    0: The repair droid hit a wall. Its position has not changed.
+    1: The repair droid has moved one step in the requested direction.
+    2: The repair droid has moved one step in the requested direction; its new position is the location of the oxygen system.
+
+You don't know anything about the area around the repair droid, but you can figure it out by watching the status codes.
+
+For example, we can draw the area using D for the droid, # for walls, . for locations the droid can traverse, and empty space for unexplored locations. Then, the initial state looks like this:
+
+      
+      
+   D  
+      
+      
+
+To make the droid go north, send it 1. If it replies with 0, you know that location is a wall and that the droid didn't move:
+
+      
+   #  
+   D  
+      
+      
+
+To move east, send 4; a reply of 1 means the movement was successful:
+
+      
+   #  
+   .D 
+      
+      
+
+Then, perhaps attempts to move north (1), south (2), and east (4) are all met with replies of 0:
+
+      
+   ## 
+   .D#
+    # 
+      
+
+Now, you know the repair droid is in a dead end. Backtrack with 3 (which you already know will get a reply of 1 because you already know that location is open):
+
+      
+   ## 
+   D.#
+    # 
+      
+
+Then, perhaps west (3) gets a reply of 0, south (2) gets a reply of 1, south again (2) gets a reply of 0, and then west (3) gets a reply of 2:
+
+      
+   ## 
+  #..#
+  D.# 
+   #  
+
+Now, because of the reply of 2, you know you've found the oxygen system! In this example, it was only 2 moves away from the repair droid's starting position.
+
+What is the fewest number of movement commands required to move the repair droid from its starting position to the location of the oxygen system?
+
+--- Part Two ---
+
+You quickly repair the oxygen system; oxygen gradually fills the area.
+
+Oxygen starts in the location containing the repaired oxygen system. It takes one minute for oxygen to spread to all open locations that are adjacent to a location that already contains oxygen. Diagonal locations are not adjacent.
+
+In the example above, suppose you've used the droid to explore the area fully and have the following map (where locations that currently contain oxygen are marked O):
+
+ ##   
+#..## 
+#.#..#
+#.O.# 
+ ###  
+
+Initially, the only location which contains oxygen is the location of the repaired oxygen system. However, after one minute, the oxygen spreads to all open (.) locations that are adjacent to a location containing oxygen:
+
+ ##   
+#..## 
+#.#..#
+#OOO# 
+ ###  
+
+After a total of two minutes, the map looks like this:
+
+ ##   
+#..## 
+#O#O.#
+#OOO# 
+ ###  
+
+After a total of three minutes:
+
+ ##   
+#O.## 
+#O#OO#
+#OOO# 
+ ###  
+
+And finally, the whole region is full of oxygen after a total of four minutes:
+
+ ##   
+#OO## 
+#O#OO#
+#OOO# 
+ ###  
+
+So, in this example, all locations contain oxygen after 4 minutes.
+
+Use the repair droid to get a complete map of the area. How many minutes will it take to fill with oxygen?
+
+"""
+
+class IntPuterVM:
+	
+	def __init__(self, memory):
+		self.mem = memory
+		self.mem += [0] * 5000
+		self.last_out = None
+		self.prog_in = []
+		self.halted = False
+		self.blocked = True
+		self.ip_state = 0
+		self.base = 0
+
+	def buffer_read(self, i):
+		self.prog_in.append(i)
+		self.blocked = False
+
+	def run(self):
+		ip = self.ip_state
+		while True:
+			A = None
+			B = None
+			C = None
+			d_ip = None
+			ip_updated = False
+			op = self.mem[ip]
+			modes, opcode = str(op)[:-2], int(str(op)[-2:])
+			if opcode == 99: # Halt
+				self.halted = True
+				return
+			if opcode == 1: # res = C + B
+				d_ip = 4
+				if modes == '':
+					self.mem[self.mem[ip+3]] = self.mem[self.mem[ip+1]] + self.mem[self.mem[ip+2]]
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					modes = modes[:-1]
+					if modes == '' or modes[-1] == '0':
+						self.mem[self.mem[ip+3]] = C + B
+					elif modes == '2':
+						self.mem[self.base+self.mem[ip+3]] = C + B
+			elif opcode == 2: # res = C * B
+				d_ip = 4
+				if modes == '':
+					self.mem[self.mem[ip+3]] = self.mem[self.mem[ip+1]] * self.mem[self.mem[ip+2]]
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					modes = modes[:-1]
+					if modes == '' or modes[-1] == '0':
+						self.mem[self.mem[ip+3]] = C * B
+					elif modes[-1] == '2':
+						self.mem[self.base+self.mem[ip+3]] = C * B
+			elif opcode == 3: # Store input at param 1
+				d_ip = 2
+				if len(self.prog_in) == 0:
+					self.blocked = True
+					self.ip_state = ip
+					return
+				if modes == '' or modes == '0':
+					self.mem[self.mem[ip+1]] = self.prog_in.pop(0)
+				elif modes == '1':
+					self.mem[ip+1] = self.prog_in.pop(0)
+				elif modes == '2':
+					self.mem[self.base+self.mem[ip+1]] = self.prog_in.pop(0)
+			elif opcode == 4: # Output value at param 1
+				d_ip = 2
+				if modes == '' or modes == '0':
+					self.last_out = self.mem[self.mem[ip+1]]
+				elif modes == '1':
+					self.last_out = self.mem[ip+1]
+				elif modes == '2':
+					self.last_out = self.mem[self.base+self.mem[ip+1]]
+				yield self.last_out
+			elif opcode == 5: # Jump if True (non-zero)
+				d_ip = 3
+				if modes == '':
+					if self.mem[self.mem[ip+1]] != 0:
+						ip = self.mem[self.mem[ip+2]]
+						ip_updated = True
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					modes = modes[:-1]
+					if C != 0:
+						ip = B
+						ip_updated = True
+			elif opcode == 6: # Jump if False (zero)
+				d_ip = 3
+				if modes == '':
+					if self.mem[self.mem[ip+1]] == 0:
+						ip = self.mem[self.mem[ip+2]]
+						ip_updated = True
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					modes = modes[:-1]
+					if C == 0:
+						ip = B
+						ip_updated = True
+			elif opcode == 7: # res = C < B
+				d_ip = 4
+				if modes == '':
+					if self.mem[self.mem[ip+1]] < self.mem[self.mem[ip+2]]:
+						self.mem[self.mem[ip+3]] = 1
+					else:
+						self.mem[self.mem[ip+3]] = 0
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					modes = modes[:-1]
+					if C < B:
+						if modes == '' or modes[-1] == '0':
+							self.mem[self.mem[ip+3]] = 1
+						elif modes[-1] == '2':
+							self.mem[self.base+self.mem[ip+3]] = 1
+					else:
+						if modes == '' or modes[-1] == '0':
+							self.mem[self.mem[ip+3]] = 0
+						elif modes[-1] == '2':
+							self.mem[self.base+self.mem[ip+3]] = 0
+			elif opcode == 8: # res = C == B
+				d_ip = 4
+				if modes == '':
+					if self.mem[self.mem[ip+1]] == self.mem[self.mem[ip+2]]:
+						self.mem[self.mem[ip+3]] = 1
+					else:
+						self.mem[self.mem[ip+3]] = 0
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					modes = modes[:-1]
+					if C == B:
+						if modes == '' or modes[-1] == '0':
+							self.mem[self.mem[ip+3]] = 1
+						elif modes[-1] == '2':
+							self.mem[self.base+self.mem[ip+3]] = 1
+					else:
+						if modes == '' or modes[-1] == '0':
+							self.mem[self.mem[ip+3]] = 0
+						elif modes[-1] == '2':
+							self.mem[self.base+self.mem[ip+3]] = 0
+			elif opcode == 9: # adjust base pointer by param
+				d_ip = 2
+				if modes == '':
+					self.base += self.mem[self.mem[ip+1]]
+				else:
+					if modes[-1] == '1':
+						self.base += self.mem[ip+1]
+					elif modes[-1] == '0':
+						self.base += self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						self.base += self.mem[self.base+self.mem[ip+1]]
+			if not ip_updated:
+				ip += d_ip
+
+world_map = dict()
+search_buffer = list()
+def discover_world():
+	global world_map
+	global search_buffer
+	while len(search_buffer) > 0:
+		vm, pos, steps = search_buffer.pop(0)
+		x, y = pos
+		n_vm = deepcopy(vm)
+		n_vm.buffer_read(1)
+		s_vm = deepcopy(vm)
+		s_vm.buffer_read(2)
+		e_vm = deepcopy(vm)
+		e_vm.buffer_read(3)
+		w_vm = deepcopy(vm)
+		w_vm.buffer_read(4)
+		if (x, y+1) not in world_map:
+			for reply in n_vm.run():
+				world_map[(x,y+1)] = reply
+				if reply == 1:
+					search_buffer.append((n_vm, (x,y+1), steps+1))
+				elif reply == 2:
+					return steps+1
+		if (x, y-1) not in world_map:
+			for reply in s_vm.run():
+				world_map[(x,y-1)] = reply
+				if reply == 1:
+					search_buffer.append((s_vm, (x,y-1), steps+1))
+				elif reply == 2:
+					return steps+1
+		if (x+1, y) not in world_map:
+			for reply in e_vm.run():
+				world_map[(x+1,y)] = reply
+				if reply == 1:
+					search_buffer.append((e_vm, (x+1,y), steps+1))
+				elif reply == 2:
+					return steps+1
+		if (x-1, y) not in world_map:
+			for reply in w_vm.run():
+				world_map[(x-1,y)] = reply
+				if reply == 1 and (x-1,y):
+					search_buffer.append((w_vm, (x-1,y), steps+1))
+				elif reply == 2:
+					return steps+1
+
+if __name__ == "__main__":
+
+	# Part 1 Solution
+	with open("day15_input", 'r') as infile:
+		prog = [ int(x) for x in infile.readline().strip().split(',') ]
+	vm = IntPuterVM(prog[:])
+	world_map[(0,0)] = 1
+	search_buffer.append((vm, (0,0), 0))
+	print(discover_world())
+
