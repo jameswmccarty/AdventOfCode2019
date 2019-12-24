88f1080e9b555ab89cb2edf9c1e35757c729e2f7
James McCarty <jameswmccarty@gmail.com>
2019-12-24 18:27:48 -0500

    Update day 20 file (Part 2)

--- day20.py
+++ day20.py
@@ -212,6 +212,7 @@ In your maze, when accounting for recursion, how many steps does it take to get 
 """
 world = dict()
 portals = dict()
+outers = dict()
 
 def parse_line(idx, line):
 	global world
@@ -259,7 +260,7 @@ def warp_point(pos):
 	for exit in portals.keys():
 		if portals[exit] == name and exit != pos:
 			return exit
-	return None
+	return None	
 
 def path_search(start, finish):
 	global world
@@ -296,6 +297,87 @@ def path_search(start, finish):
 						heapq.heappush(h, (steps + delta, coord))
 	return min_search
 
+def warp_point_rec(pos, depth):
+	if pos not in portals:
+		return None
+	name = portals[pos]
+	if (name == 'ZZ' or name == 'AA') and depth > 0:
+		return None
+	if depth == 0 and pos in outers:
+		return None
+	for exit in portals.keys():
+		if portals[exit] == name and exit != pos:
+			return exit
+	return None
+
+# return True if portal is an 'outside' portal
+def is_outer(pos):
+	loc = pos
+	h = []
+	heapq.heappush(h, loc)
+	seen = set()
+	seen.add(loc)
+	while len(h) > 0:
+		loc = heapq.heappop(h)
+		x, y = loc
+		next_locs = []
+		next_locs.append((x-1, y))
+		next_locs.append((x, y-1))
+		next_locs.append((x+1, y))
+		next_locs.append((x, y+1))
+		for next_loc in next_locs:
+			if next_loc not in world: # Fell off the map
+				return True
+			else:
+				path = world[next_loc]
+				if (path == ' ' or ('A' <= path <= 'Z')) and next_loc not in seen:
+					seen.add(next_loc)
+					heapq.heappush(h, next_loc)
+	return False
+
+def path_search_rec(start, finish):
+	global world
+	# (steps, (x,y), level)
+	loc    = start
+	level  = 0
+	steps  = 0 # Time spent searching
+	h = []
+	heapq.heappush(h, (steps, loc, level) )
+	min_search = float('inf')
+	seen = set()
+	seen.add((loc, level))
+	while len(h) > 0 and h[0][0] <= min_search:
+		steps, loc, level = heapq.heappop(h)
+		x, y = loc
+		if loc == finish and level == 0:
+			min_search = min(min_search, steps)			
+		else: # still possible shorter path
+			next_locs = []
+			if (x-1,y) in world:
+				next_locs.append((x-1, y))
+			if (x,y-1) in world:
+				next_locs.append((x, y-1))
+			if (x+1,y) in world:
+				next_locs.append((x+1, y))
+			if (x,y+1) in world:
+				next_locs.append((x, y+1))
+			for next_loc in next_locs:
+					path = world[next_loc]		
+					if path == '.' and (next_loc, level) not in seen:
+						seen.add((next_loc, level))
+						heapq.heappush(h, (steps + 1, next_loc, level))
+			next_loc = warp_point_rec(loc, level)
+			if next_loc != None:
+				if loc in outers:
+					level -= 1
+				else:
+					level += 1
+				path = world[next_loc]
+				if path == '.' and (next_loc, level) not in seen:
+					seen.add((next_loc, level))
+					heapq.heappush(h, (steps + 1, next_loc, level))
+
+	return min_search
 
 if __name__ == "__main__":
 
@@ -315,3 +397,8 @@ if __name__ == "__main__":
 			finish = k
 	print(path_search(start, finish))
 
+	# Part 2 Solution
+	for k, v in portals.items():
+		if is_outer(k):
+			outers[k] = v
+	print(path_search_rec(start, finish))
