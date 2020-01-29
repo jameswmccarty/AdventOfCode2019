a3b600639d8c394a581ce250671620590a7a42c3
James McCarty <jameswmccarty@gmail.com>
2020-01-28 20:13:39 -0500

    Update with progress

--- day22.py
+++ day22.py
@@ -165,6 +165,7 @@ Although it hasn't changed, you can still get your puzzle input.
 """
 
 deck = None
+decksize = None
 
 def cut(N):
 	global deck
@@ -195,6 +196,34 @@ def parse_line(line):
 		print("Error:", line)
 		exit()
 
+def cut_2(i, N):
+	global decksize
+	return ((decksize-N)+i) % decksize
+
+def deal_2(i, N):
+	global decksize
+	return (i*N) % decksize
+
+def new_stack_2(i):
+	global decksize
+	return (decksize - i - 1) % decksize
+
+def parse_line_2(line, i):
+	line = line.split(' ')
+	if line[0] == 'cut':
+		i = cut_2(i, int(line[-1]))
+	elif line[0] == 'deal' and line[-1] == 'stack':
+		i = new_stack_2(i)
+	elif line[0] == 'deal':
+		i = deal_2(i, int(line[-1]))
+	else:
+		print("Error:", line)
+		exit()
+	return i
+
+def unshuffle(n):
+	return ((n - 140568611385) / 6871724559536) + 2
+
 if __name__ == "__main__":
 
 	# Part 1 Solution
@@ -205,4 +234,40 @@ if __name__ == "__main__":
 			parse_line(line.strip())
 	print(deck.index(2019))
 
-	
+	"""
+	place = 2019
+	decksize = 10007
+	with open("day22_input", 'r') as infile:
+		for line in infile.readlines():
+			place = parse_line_2(line.strip(), place)
+	print(place)
+	"""
+
+	# Part 2 Solution
+
+	"""
+	big_size = 119315717514047
+	num_shuffle = 101741582076661
+	shuff_seq = []
+	place = 0
+	decksize = big_size
+	with open("day22_input", 'r') as infile:
+		for line in infile.readlines():
+			shuff_seq.append(line.strip())
+	shuff_seq.reverse()
+	for z in range(250):
+		place = z
+		for step in shuff_seq:
+			place = parse_line_2(step, place)
+		print(z, place)
+
+
+	# Place				Delta		Difference
+	# 0	105712837006360		
+	# 1	112584561565896		
+	# 2	140568611385		
+	# 3	7012293170921	6871724559536	140568611385
+	# 4	13884017730457	6871724559536	
+
+	# Linear sequence: P1 = ((P0 - 2) * 6871724559536 + 140568611385) % 119315717514047
+	"""
