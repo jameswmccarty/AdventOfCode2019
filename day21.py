0b5ee40333213c2e97f8420f400d0cf07f8cba8f
James McCarty <jameswmccarty@gmail.com>
2020-02-02 19:19:45 -0500

    Add Day 21 Files

---
+++ day21.py
@@ -0,0 +1,394 @@
+#!/usr/bin/python
+
+"""
+--- Day 21: Springdroid Adventure ---
+
+You lift off from Pluto and start flying in the direction of Santa.
+
+While experimenting further with the tractor beam, you accidentally pull an asteroid directly into your ship! It deals significant damage to your hull and causes your ship to begin tumbling violently.
+
+You can send a droid out to investigate, but the tumbling is causing enough artificial gravity that one wrong step could send the droid through a hole in the hull and flying out into space.
+
+The clear choice for this mission is a droid that can jump over the holes in the hull - a springdroid.
+
+You can use an Intcode program (your puzzle input) running on an ASCII-capable computer to program the springdroid. However, springdroids don't run Intcode; instead, they run a simplified assembly language called springscript.
+
+While a springdroid is certainly capable of navigating the artificial gravity and giant holes, it has one downside: it can only remember at most 15 springscript instructions.
+
+The springdroid will move forward automatically, constantly thinking about whether to jump. The springscript program defines the logic for this decision.
+
+Springscript programs only use Boolean values, not numbers or strings. Two registers are available: T, the temporary value register, and J, the jump register. If the jump register is true at the end of the springscript program, the springdroid will try to jump. Both of these registers start with the value false.
+
+Springdroids have a sensor that can detect whether there is ground at various distances in the direction it is facing; these values are provided in read-only registers. Your springdroid can detect ground at four distances: one tile away (A), two tiles away (B), three tiles away (C), and four tiles away (D). If there is ground at the given distance, the register will be true; if there is a hole, the register will be false.
+
+There are only three instructions available in springscript:
+
+    AND X Y sets Y to true if both X and Y are true; otherwise, it sets Y to false.
+    OR X Y sets Y to true if at least one of X or Y is true; otherwise, it sets Y to false.
+    NOT X Y sets Y to true if X is false; otherwise, it sets Y to false.
+
+In all three instructions, the second argument (Y) needs to be a writable register (either T or J). The first argument (X) can be any register (including A, B, C, or D).
+
+For example, the one-instruction program NOT A J means "if the tile immediately in front of me is not ground, jump".
+
+Or, here is a program that jumps if a three-tile-wide hole (with ground on the other side of the hole) is detected:
+
+NOT A J
+NOT B T
+AND T J
+NOT C T
+AND T J
+AND D J
+
+The Intcode program expects ASCII inputs and outputs. It will begin by displaying a prompt; then, input the desired instructions one per line. End each line with a newline (ASCII code 10). When you have finished entering your program, provide the command WALK followed by a newline to instruct the springdroid to begin surveying the hull.
+
+If the springdroid falls into space, an ASCII rendering of the last moments of its life will be produced. In these, @ is the springdroid, # is hull, and . is empty space. For example, suppose you program the springdroid like this:
+
+NOT D J
+WALK
+
+This one-instruction program sets J to true if and only if there is no ground four tiles away. In other words, it attempts to jump into any hole it finds:
+
+.................
+.................
+@................
+#####.###########
+
+.................
+.................
+.@...............
+#####.###########
+
+.................
+..@..............
+.................
+#####.###########
+
+...@.............
+.................
+.................
+#####.###########
+
+.................
+....@............
+.................
+#####.###########
+
+.................
+.................
+.....@...........
+#####.###########
+
+.................
+.................
+.................
+#####@###########
+
+However, if the springdroid successfully makes it across, it will use an output instruction to indicate the amount of damage to the hull as a single giant integer outside the normal ASCII range.
+
+Program the springdroid with logic that allows it to survey the hull without falling into space. What amount of hull damage does it report?
+
+To begin, get your puzzle input.
+
+--- Part Two ---
+
+There are many areas the springdroid can't reach. You flip through the manual and discover a way to increase its sensor range.
+
+Instead of ending your springcode program with WALK, use RUN. Doing this will enable extended sensor mode, capable of sensing ground up to nine tiles away. This data is available in five new read-only registers:
+
+    Register E indicates whether there is ground five tiles away.
+    Register F indicates whether there is ground six tiles away.
+    Register G indicates whether there is ground seven tiles away.
+    Register H indicates whether there is ground eight tiles away.
+    Register I indicates whether there is ground nine tiles away.
+
+All other functions remain the same.
+
+Successfully survey the rest of the hull by ending your program with RUN. What amount of hull damage does the springdroid now report?
+
+Although it hasn't changed, you can still get your puzzle input.
+
+"""
+
+class IntPuterVM:
+	
+	def __init__(self, memory):
+		self.mem = memory
+		self.mem += [0] * 5000
+		self.last_out = None
+		self.prog_in = []
+		self.halted = False
+		self.blocked = True
+		self.ip_state = 0
+		self.base = 0
+
+	def buffer_read(self, i):
+		self.prog_in.append(i)
+		self.blocked = False
+
+	def ascii_read(self, cmd):
+		for char in cmd:
+			self.buffer_read(ord(char))
+		self.buffer_read(10)
+
+	def run(self):
+		ip = self.ip_state
+		while True:
+			A = None
+			B = None
+			C = None
+			d_ip = None
+			ip_updated = False
+			op = self.mem[ip]
+			modes, opcode = str(op)[:-2], int(str(op)[-2:])
+			if opcode == 99: # Halt
+				self.halted = True
+				return
+			if opcode == 1: # res = C + B
+				d_ip = 4
+				if modes == '':
+					self.mem[self.mem[ip+3]] = self.mem[self.mem[ip+1]] + self.mem[self.mem[ip+2]]
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					modes = modes[:-1]
+					if modes == '' or modes[-1] == '0':
+						self.mem[self.mem[ip+3]] = C + B
+					elif modes == '2':
+						self.mem[self.base+self.mem[ip+3]] = C + B
+			elif opcode == 2: # res = C * B
+				d_ip = 4
+				if modes == '':
+					self.mem[self.mem[ip+3]] = self.mem[self.mem[ip+1]] * self.mem[self.mem[ip+2]]
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					modes = modes[:-1]
+					if modes == '' or modes[-1] == '0':
+						self.mem[self.mem[ip+3]] = C * B
+					elif modes[-1] == '2':
+						self.mem[self.base+self.mem[ip+3]] = C * B
+			elif opcode == 3: # Store input at param 1
+				d_ip = 2
+				if len(self.prog_in) == 0:
+					self.blocked = True
+					self.ip_state = ip
+					return
+				if modes == '' or modes == '0':
+					self.mem[self.mem[ip+1]] = self.prog_in.pop(0)
+				elif modes == '1':
+					self.mem[ip+1] = self.prog_in.pop(0)
+				elif modes == '2':
+					self.mem[self.base+self.mem[ip+1]] = self.prog_in.pop(0)
+			elif opcode == 4: # Output value at param 1
+				d_ip = 2
+				if modes == '' or modes == '0':
+					self.last_out = self.mem[self.mem[ip+1]]
+				elif modes == '1':
+					self.last_out = self.mem[ip+1]
+				elif modes == '2':
+					self.last_out = self.mem[self.base+self.mem[ip+1]]
+				yield self.last_out
+			elif opcode == 5: # Jump if True (non-zero)
+				d_ip = 3
+				if modes == '':
+					if self.mem[self.mem[ip+1]] != 0:
+						ip = self.mem[self.mem[ip+2]]
+						ip_updated = True
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					modes = modes[:-1]
+					if C != 0:
+						ip = B
+						ip_updated = True
+			elif opcode == 6: # Jump if False (zero)
+				d_ip = 3
+				if modes == '':
+					if self.mem[self.mem[ip+1]] == 0:
+						ip = self.mem[self.mem[ip+2]]
+						ip_updated = True
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					modes = modes[:-1]
+					if C == 0:
+						ip = B
+						ip_updated = True
+			elif opcode == 7: # res = C < B
+				d_ip = 4
+				if modes == '':
+					if self.mem[self.mem[ip+1]] < self.mem[self.mem[ip+2]]:
+						self.mem[self.mem[ip+3]] = 1
+					else:
+						self.mem[self.mem[ip+3]] = 0
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					modes = modes[:-1]
+					if C < B:
+						if modes == '' or modes[-1] == '0':
+							self.mem[self.mem[ip+3]] = 1
+						elif modes[-1] == '2':
+							self.mem[self.base+self.mem[ip+3]] = 1
+					else:
+						if modes == '' or modes[-1] == '0':
+							self.mem[self.mem[ip+3]] = 0
+						elif modes[-1] == '2':
+							self.mem[self.base+self.mem[ip+3]] = 0
+			elif opcode == 8: # res = C == B
+				d_ip = 4
+				if modes == '':
+					if self.mem[self.mem[ip+1]] == self.mem[self.mem[ip+2]]:
+						self.mem[self.mem[ip+3]] = 1
+					else:
+						self.mem[self.mem[ip+3]] = 0
+				else:
+					if modes[-1] == '1':
+						C = self.mem[ip+1]
+					elif modes[-1] == '0':
+						C = self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						C = self.mem[self.base+self.mem[ip+1]]
+					modes = modes[:-1]
+					if modes == '':
+						modes = '0'
+					if modes[-1] == '1':
+						B = self.mem[ip+2]
+					elif modes[-1] == '0':
+						B = self.mem[self.mem[ip+2]]
+					elif modes[-1] == '2':
+						B = self.mem[self.base+self.mem[ip+2]]
+					modes = modes[:-1]
+					if C == B:
+						if modes == '' or modes[-1] == '0':
+							self.mem[self.mem[ip+3]] = 1
+						elif modes[-1] == '2':
+							self.mem[self.base+self.mem[ip+3]] = 1
+					else:
+						if modes == '' or modes[-1] == '0':
+							self.mem[self.mem[ip+3]] = 0
+						elif modes[-1] == '2':
+							self.mem[self.base+self.mem[ip+3]] = 0
+			elif opcode == 9: # adjust base pointer by param
+				d_ip = 2
+				if modes == '':
+					self.base += self.mem[self.mem[ip+1]]
+				else:
+					if modes[-1] == '1':
+						self.base += self.mem[ip+1]
+					elif modes[-1] == '0':
+						self.base += self.mem[self.mem[ip+1]]
+					elif modes[-1] == '2':
+						self.base += self.mem[self.base+self.mem[ip+1]]
+			if not ip_updated:
+				ip += d_ip
+
+if __name__ == "__main__":
+
+	# Part 1 Solution
+	with open("day21_input", 'r') as infile:
+		prog = [ int(x) for x in infile.readline().strip().split(',') ]
+	vm = IntPuterVM(prog[:])
+	for out in vm.run():
+		print(chr(out), end='')
+
+	"""
+		-- Jumps are 4 steps
+		-- Make sure that D is solid
+ 		-- Jump if hole at 3rd spot, or directly in front
+	"""
+	vm.ascii_read("NOT C J")
+	vm.ascii_read("AND D J")
+	vm.ascii_read("NOT A T")
+	vm.ascii_read("OR T J")
+	vm.ascii_read("WALK")
+	for out in vm.run():
+		if out > 127:
+			print(int(out))
+		else:
+			print(chr(out), end='')
+
+	# Part 2 Solution
+	vm = IntPuterVM(prog[:])
+	for out in vm.run():
+		print(chr(out), end='')
+	vm.ascii_read("NOT C J")
+	vm.ascii_read("AND D J")
+	vm.ascii_read("AND H J")
+	vm.ascii_read("NOT B T")
+	vm.ascii_read("AND D T")
+	vm.ascii_read("OR T J")
+	vm.ascii_read("NOT A T")
+	vm.ascii_read("OR T J")
+	vm.ascii_read("RUN")
+	for out in vm.run():
+		if out > 127:
+			print(int(out))
+		else:
+			print(chr(out), end='')
