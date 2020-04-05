3528d437c12f71b7ec2a50f4d7b0fe65ed8bf3e4
James McCarty <jameswmccarty@gmail.com>
2020-04-05 12:52:41 -0400

    Update Day 16 (Part 2)

--- day16.py
+++ day16.py
@@ -127,5 +127,26 @@ if __name__ == "__main__":
 	n = [ int(x) for x in n.strip() ]
 	print(''.join(str(i) for i in FFT(n,100)[0:8]))
 
+	# Part 2 Solution
+	"""
+	Technique borrowed from /u/Diderikdm/ on AOC reddit post.
+	"""
+	with open('day16_input', 'r') as infile:
+		n = infile.readline().strip()*10000
+	i = (n[int(n[0:7]):])
+	for a in range(100):
+		string = '' 
+		e = 0
+		while e < len(i):
+		    if e == 0:
+		        total = 0
+		        for f in i:
+		            total += int(f)
+		    elif e > 0:
+		        total -= int(i[e-1])
+		    string += str(total)[-1]
+		    e += 1
+		i = string
+	print(i[0:8]) 
 
 
